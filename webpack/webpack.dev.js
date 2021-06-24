6cacdf9d1405b332f2420b271f8470f828f2d3a0
SerhatG35 <genc.serhat97@gmail.com>
2021-06-24 22:16:34 +0300

    first commit

---
+++ webpack/webpack.dev.js
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable no-undef */
+const webpack = require('webpack')
+
+module.exports = {
+    mode: 'development',
+    devServer: {
+        open:true
+    },
+    devtool: 'cheap-module-source-map'
+}
+
---
+++ webpack/webpack.prod.js
@@ -0,0 +1,8 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable no-undef */
+const webpack = require('webpack')
+
+module.exports = {
+    mode: 'production',
+    devtool: 'source-map',
+}
